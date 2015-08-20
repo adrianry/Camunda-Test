55a38057b028879108fc1e2250cada1e7b10b708
Adrian <ryser@netdot.ch>
2015-08-20 22:52:08 +0200

    Field injection und user task

---
+++ src/main/java/org/camunda/bpm/getstarted/loanapproval/Methodenaufruf.java
@@ -0,0 +1,25 @@
+package org.camunda.bpm.getstarted.loanapproval;
+
+import java.util.logging.Logger;
+
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.Expression;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class Methodenaufruf implements JavaDelegate {
+	private final static Logger LOGGER = Logger.getLogger(ProcessRequestDelegate.class.getName());
+	private Expression InputFeld;
+	
+	  public void execute(DelegateExecution execution) throws Exception {
+	    try {
+	    	String inputFeld = (String)InputFeld.getValue(execution);
+	    	
+	    	 LOGGER.info("\n\n\n Klasse Methodenaufruf meldet: " + inputFeld +"\n\n\n");
+	    			 
+	    			 
+	    } catch (Exception ex) {
+	       // throw new BpmnError();
+	    }
+	  }
+
+	}
